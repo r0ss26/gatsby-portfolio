9fff945f65196905665a33826150452dd58deec5
Ross Baker <rossbaker32@gmail.com>
2020-07-04 22:35:28 +1000

    Adds a component for the homepage call to action

---
+++ src/components/ViewWork/ViewWork.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './ViewWork.module.css';
+
+const ViewWork = () => {
+  return (
+    <div className={styles.container}>
+      <h2>View my Work</h2>
+      <button className={styles.button}>Here.</button>
+    </div>
+  )
+}
+
+export default ViewWork
+
+
+// judah
+// github 
+// lincoln moraga 
