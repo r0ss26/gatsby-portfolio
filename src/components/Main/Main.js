1aa7c7e3e1df1106d8a8120a030c464bf8dd3eb0
Ross Baker <rossbaker32@gmail.com>
2020-07-05 22:17:15 +1000

    Changes layout of home page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -11,22 +11,26 @@ import githubIcon from '../../icons/social/icons8-github.svg'
 import linkedinIcon from '../../icons/social/icons8-linkedin.svg'
 import mediumIcon from '../../icons/social/icons8-medium.svg'
 import twitterIcon from '../../icons/social/icons8-twitter.svg'
+import MobileNav from '../MobileNav/MobileNav';
 
 const Main = () => {
   return (
-    <main>
-      <IntroCard />
-      <ViewWork>
-        <div>
-          <NeumorphicIcon imgSrc={githubIcon} altText="GitHub Logo" linkUrl="https://github.com/r0ss26" />
-          <NeumorphicIcon imgSrc={twitterIcon} altText="Twitter Logo" linkUrl="https://twitter.com/rossandrewbaker" />
-          <NeumorphicIcon imgSrc={mediumIcon} altText="Medium Logo" linkUrl="https://medium.com/@rossabaker" />
-          <NeumorphicIcon imgSrc={linkedinIcon} altText="LinkedIn Logo" linkUrl="https://www.linkedin.com/in/rossandrewbaker/" />
-          <NeumorphicIcon imgSrc={emailIcon} altText="Gmail Logo" linkUrl="mailto:rossbaker32@gmail.com" />
-        </div>
-        </ViewWork>
-      <Nav />
-    </main>
+    <>
+      <main>
+        <IntroCard />
+        <ViewWork>
+          <div>
+            <NeumorphicIcon tooltip tooltipText="GitHub" imgSrc={githubIcon} altText="GitHub Logo" linkUrl="https://github.com/r0ss26" />
+            <NeumorphicIcon tooltip tooltipText="Twitter" imgSrc={twitterIcon} altText="Twitter Logo" linkUrl="https://twitter.com/rossandrewbaker" />
+            <NeumorphicIcon tooltip tooltipText="Medium" imgSrc={mediumIcon} altText="Medium Logo" linkUrl="https://medium.com/@rossabaker" />
+            <NeumorphicIcon tooltip tooltipText="LinkedIn" imgSrc={linkedinIcon} altText="LinkedIn Logo" linkUrl="https://www.linkedin.com/in/rossandrewbaker/" />
+            <NeumorphicIcon tooltip tooltipText="Email Me" imgSrc={emailIcon} altText="Gmail Logo" linkUrl="mailto:rossbaker32@gmail.com" />
+          </div>
+          </ViewWork>
+        <Nav />
+      </main>
+      <MobileNav />
+    </>
   )
 }
 
