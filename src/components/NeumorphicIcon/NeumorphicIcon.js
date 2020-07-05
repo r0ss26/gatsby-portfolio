ce15f87132944747c406185c76b486887db30f79
Ross Baker <rossbaker32@gmail.com>
2020-07-05 22:18:34 +1000

    Adds a tooltip to the icon component and props for customising

--- src/components/NeumorphicIcon/NeumorphicIcon.js
+++ src/components/NeumorphicIcon/NeumorphicIcon.js
@@ -3,14 +3,18 @@ import styles from './NeumorphicIcon.module.css';
 
 const NeumorphicIcon = (props) => {
   return (
-    <span>
-      <a target="__blank" href={props.linkUrl}>
-        <img
-          src={props.imgSrc}
-          alt={props.altText}
-        />
-      </a>
-    </span>
+    <>
+      <span className={styles.icon}>
+        <a target="__blank" href={props.linkUrl}>
+          <img
+            src={props.imgSrc}
+            alt={props.altText}
+          />
+        </a>
+        {props.tooltip && 
+          <span className={styles.tooltip}>{props.tooltipText}</span>}
+      </span>
+    </>
   )
 }
 
