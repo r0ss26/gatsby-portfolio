836610ad627caf8ca1f57d92d8138423decf5ff5
Ross Baker <rossbaker32@gmail.com>
2021-08-22 14:49:40 +1000

    Adds https tag for ogp

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -15,9 +15,10 @@ const Layout = ({ topNav, children }) => {
           content="View Ross Baker's software development portfolio. Ross Baker is
         a web developer utilisng JavaScript, React, Ruby on Rails, HTML5, CSS3 and other technologies."
         />
-        <meta property="og:image" content={typeof window === 'object' ? `${window.location.href}featured_image.png` : 'https://www.rossbaker.me/featured_image.png'} /> 
+        <meta property="og:image:secure_url" content={typeof window === 'object' ? `${window.location.href}featured_image.png` : 'https://www.rossbaker.me/featured_image.png'} /> 
         <meta property="og:image:width" content="300" /> 
         <meta property="og:image:height" content="300" /> 
+        <meta property="og:image:type" content="image/png" /> 
         <html lang="en" />
       </Helmet>
       {topNav && <TopNavBar />}
