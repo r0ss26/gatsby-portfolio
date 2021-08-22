1307f6afa368f97653bc4de99051ba8e1a0e2cf0
Ross Baker <rossbaker32@gmail.com>
2021-08-22 16:42:08 +1000

    Moves helmet before styles

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,24 +7,26 @@ import TopNavBar from "../TopNavBar/TopNavBar"
 
 const Layout = ({ topNav, children }) => {
   return (
+    <>
+    <Helmet>
+      <title>Ross' Developer Portfolio</title>
+      <meta
+        name="description"
+        content="View Ross Baker's software development portfolio. Ross Baker is
+                a web developer utilisng JavaScript, React, Ruby on Rails, HTML5, CSS3 and other technologies."
+      />
+      <meta property="og:image:secure_url" content={typeof window === 'object' ? `${window.location.href}featured_image.png` : 'https://www.rossbaker.me/featured_image.png'} /> 
+      <meta property="og:image:width" content="300" /> 
+      <meta property="og:image:height" content="300" /> 
+      <meta property="og:image:type" content="image/png" /> 
+      <html lang="en" />
+    </Helmet>
     <div className={styles.layout}>
-      <Helmet>
-        <title>Ross' Developer Portfolio</title>
-        <meta
-          name="description"
-          content="View Ross Baker's software development portfolio. Ross Baker is
-        a web developer utilisng JavaScript, React, Ruby on Rails, HTML5, CSS3 and other technologies."
-        />
-        <meta property="og:image:secure_url" content={typeof window === 'object' ? `${window.location.href}featured_image.png` : 'https://www.rossbaker.me/featured_image.png'} /> 
-        <meta property="og:image:width" content="300" /> 
-        <meta property="og:image:height" content="300" /> 
-        <meta property="og:image:type" content="image/png" /> 
-        <html lang="en" />
-      </Helmet>
       {topNav && <TopNavBar />}
       {children}
       <MobileNav />
     </div>
+    </>
   )
 }
 
