a416f74c2acc4349aa2d458d1ebea3a0d9e1e20a
Ross Baker <rossbaker@deputy.com>
2024-09-28 22:37:15 +1000

    styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,13 @@ import React from "react"
 import NeumorphicContainer from "../components/NeumorphicContainer/NeumorphicContainer"
 import Layout from "../components/Layout/Layout"
 import AboutContent from "../components/AboutContent/AboutContent.js"
+import TopNavBar from "../components/TopNavBar/TopNavBar"
 
 const About = () => {
   return (
-    <Layout topNav>
+    <Layout>
       <NeumorphicContainer>
+        <TopNavBar />
         <AboutContent />
       </NeumorphicContainer>
     </Layout>
